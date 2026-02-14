8c978d95a06f08fd3dc64643dfe43edb663dad7e
Rohan-14-1 <rohanmdl2003@gmail.com>
2026-02-15 00:02:14 +0545

    form

--- contact.js
+++
@@ -1,5 +0,0 @@
-// Form animation (fake submit effect)
-document.querySelector(".contact-form").addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Message sent successfully!");
-});
